ee99e52af3c1c5cc6a07ea509556a66d0f5ad651
Ssz990220 <39759334+Ssz990220@users.noreply.github.com>
2021-09-24 19:38:09 +0800

    change robot import path

--- test/test_load_robot.m
+++ test/test_load_robot.m
@@ -1,5 +1,5 @@
 clear;
 clc;
 close all;
-robot = importrobot('universalUR10.urdf',"MeshPath",["../ur_description/ur10/collision","/ur_description/ur10/visual"]);
+robot = importrobot('universalUR10.urdf',"MeshPath",["../asset/ur_description/ur10/collision","../asset/ur_description/ur10/visual"]);
 show(robot,'visuals','off','collision','on')
