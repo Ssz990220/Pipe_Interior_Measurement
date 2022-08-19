8cbae5e82396c3c7887dd5730d4e06ffa45ff5d6
Ssz990220 <39759334+Ssz990220@users.noreply.github.com>
2022-08-19 20:59:08 +0800

    Typo Fixing

--- assets/convex_mesh_converter.py
+++ assets/convex_mesh_converter.py
@@ -12,7 +12,7 @@ def main(args):
     p.vhacd(name_in, name_out, name_log, alpha=float(args.alpha), resolution=int(args.resolution))
     
 if __name__=='__main__':
-    parser = argparse.ArgumentParser(description='Convert a non-convex .obj file to multiple convex objects (but still in one single .ojb file)')
+    parser = argparse.ArgumentParser(description='Convert a non-convex .obj file to multiple convex objects (but still in one single .obj file)')
     parser.add_argument('--input',help='File path for the .obj file you want to convert.')
     parser.add_argument('--output',help='File path for the generated .obj file.')
     parser.add_argument('--log',help='Path for log file. log.txt as default', default='log.txt')
